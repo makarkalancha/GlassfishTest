cd60a22dc37910ebd733cac8b36cb2de28f2d94e
makarkalancha <makarkalancha@gmail.com>
2015-02-05 15:00:05 -0500

    first

---
+++ src/com/mycompany/NameServlet.java
@@ -0,0 +1,61 @@
+package com.mycompany;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
+import javax.json.JsonWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NameServlet
+ */
+@WebServlet("/NameServlet")
+public class NameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NameServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		JsonObjectBuilder builder = Json.createObjectBuilder();
+		builder.add(
+				"person",
+				Json.createObjectBuilder()
+				.add("firstName","Markus")
+				.add("lastName", "Eisele")
+				);
+		JsonObject result = builder.build();
+		StringWriter sw = new StringWriter();
+		try(JsonWriter writer = Json.createWriter(sw)){
+			writer.writeObject(result);
+		}
+		out.print(sw.toString());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
