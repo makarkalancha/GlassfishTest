402ccf3675d7612bdc5690525bb7bdb10c5a36ea
makarkalancha <makarkalancha@gmail.com>
2015-02-05 16:28:01 -0500

    service

---
+++ src/com/mycompany/RestApplication.java
@@ -0,0 +1,28 @@
+
+package com.mycompany;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class RestApplication extends Application{
+
+	@Override
+	public Set<Class<?>> getClasses() {
+		Set<Class<?>> classes = new HashSet<Class<?>>();
+		classes.add(SimpleService.class);
+		return classes;
+	}
+//
+//	@Override
+//	public Set<Object> getSingletons() {
+//		Set<Object> singletons = new HashSet<>();
+//		return singletons;
+//	}
+//	// all request scoped resources and providers
+	
+
+}
---
+++ src/com/mycompany/SimpleService.java
@@ -0,0 +1,26 @@
+package com.mycompany;
+
+import java.util.Date;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+//after change in class remove it from glassfish 
+//and then run as->run on server->glassfish->finish
+
+@Path("sample")
+public class SimpleService {
+	//http://localhost:8080/GlassfishTest/rest/sample/greet
+	@Path("/greet")
+	@GET
+	public String doGreet(){
+		return "Hello Stranger, the time is "+new Date();
+	}
+	
+	//http://localhost:8080/GlassfishTest/rest/sample/sayHello/bitch
+	@Path("sayHello/{name}")
+	@GET
+	public String doSayHello(@PathParam("name") String name){
+		return "Hello there, "+name;
+	}
+}
